e279dd9960fc3cd9f2808f71e4bec483a1fea09a
camilo jimenez <juliancamilojimenez2@gmail.com>
2025-09-02 20:08:46 -0500

    proyecto js

---
+++ 01-Holamundo/Holamundo.js
@@ -0,0 +1,37 @@
+console.log("Hola Mundo");
+
+let cajaDeAndy = "woody";
+
+console.log(cajaDeAndy);
+
+let edad = 19;
+let nombre = "Dabi";
+
+
+console.log("Hola, mi nombre es " + nombre + " y tengo " + edad + " años.");
+
+//Array
+
+let frutas = ["Manzana", "Pera", "Naranja"];
+
+console.log(frutas[1]);
+
+
+let frase = "javascript es la monda";
+console.log(frase.length);
+
+let fraseMayusculas = frase.toUpperCase();
+console.log(fraseMayusculas);
+
+let fraseMinusculas = frase.toLowerCase();
+console.log(fraseMinusculas);
+
+//operaciones matematicas
+
+const suma= 3 + 12;
+console.log(suma);
+
+
+
+
+
